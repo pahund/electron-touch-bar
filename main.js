2870ec44a35fd134f9f9ec6421d8c27dc61556a0
Hund <pahund@lm-ber-14501643.speedport_w_723v_1_43_000>
2017-04-09 14:44:01 +0200

    added touchbar demo from documentation
    Change-Id: I8345d6df6994cbf4823eebcbc989c3a0caa2af04

--- main.js
+++ main.js
@@ -1,10 +1,114 @@
-const { app, BrowserWindow } = require('electron');
+const electron = require('electron');
+
+function printElectron(curr, indent = 0) {
+    if (!curr) {
+        return;
+    }
+    Object.keys(curr).forEach(key => {
+        if (key.match(/chrome/gi)) {
+            console.log('!!!!!!!!!!!!!!1');
+        }
+        console.log(`${' '.repeat(indent)}${key}`);
+        if (typeof curr[key] === 'object') {
+            printElectron(curr[key], indent + 4);
+        }
+    });
+}
+
+//printElectron(electron);
+
+const { app, BrowserWindow, TouchBar } = require('electron');
+const { TouchBarLabel, TouchBarButton, TouchBarSpacer } = TouchBar;
 const path = require('path');
 const url = require('url');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let win;
+
+let spinning = false;
+
+// Reel labels
+const reel1 = new TouchBarLabel();
+const reel2 = new TouchBarLabel();
+const reel3 = new TouchBarLabel();
+
+// Spin result label
+const result = new TouchBarLabel();
+
+// Spin button
+const spin = new TouchBarButton({
+    label: '🎰 Spin',
+    backgroundColor: '#7851A9',
+    click: () => {
+        // Ignore clicks if already spinning
+        if (spinning) {
+            return
+        }
+
+        spinning = true;
+        result.label = '';
+
+        let timeout = 10;
+        const spinLength = 4 * 1000; // 4 seconds
+        const startTime = Date.now();
+
+        const spinReels = () => {
+            updateReels();
+
+            if ((Date.now() - startTime) >= spinLength) {
+                finishSpin();
+            } else {
+                // Slow down a bit on each spin
+                timeout *= 1.1;
+                setTimeout(spinReels, timeout);
+            }
+        };
+
+        spinReels()
+    }
+});
+
+const getRandomValue = () => {
+    const values = ['🍒', '💎', '7️⃣', '🍊', '🔔', '⭐', '🍇', '🍀'];
+    return values[Math.floor(Math.random() * values.length)];
+};
+
+const updateReels = () => {
+    reel1.label = getRandomValue();
+    reel2.label = getRandomValue();
+    reel3.label = getRandomValue();
+};
+
+const finishSpin = () => {
+    const uniqueValues = new Set([reel1.label, reel2.label, reel3.label]).size;
+    if (uniqueValues === 1) {
+        // All 3 values are the same
+        result.label = '💰 Jackpot!';
+        result.textColor = '#FDFF00';
+    } else if (uniqueValues === 2) {
+        // 2 values are the same
+        result.label = '😍 Winner!';
+        result.textColor = '#FDFF00';
+    } else {
+        // No values are the same
+        result.label = '🙁 Spin Again';
+        result.textColor = null;
+    }
+    spinning = false;
+};
+
+const touchBar = new TouchBar([
+    spin,
+    new TouchBarSpacer({size: 'large'}),
+    reel1,
+    new TouchBarSpacer({size: 'small'}),
+    reel2,
+    new TouchBarSpacer({size: 'small'}),
+    reel3,
+    new TouchBarSpacer({size: 'large'}),
+    result
+]);
 
 function createWindow() {
     // Create the browser window.
@@ -27,6 +131,8 @@ function createWindow() {
         // when you should delete the corresponding element.
         win = null;
     });
+
+    win.setTouchBar(touchBar);
 }
 
 // This method will be called when Electron has finished
